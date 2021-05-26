1637e9642d98e279a50c9ffc95f8fe30d0f1b976
misinayu <yama_masa22@yahoo.co.jp>
2021-05-26 23:26:11 +0900

    if文を式として扱いコードをシンプルにする

--- KotlinExample/src/main/kotlin/Example.kt
+++ KotlinExample/src/main/kotlin/Example.kt
@@ -1,3 +1,15 @@
 fun main() {
     println("Hello Kotlin.")
+    printOddOrEventNumberText(100)
+    printOddOrEventNumberText(99)
+}
+
+fun printOddOrEventNumberText(num: Int) {
+    val text = if (num % 2 == 1) {
+        "奇数"
+    } else {
+        "偶数"
+    }
+
+    println(text)
 }
